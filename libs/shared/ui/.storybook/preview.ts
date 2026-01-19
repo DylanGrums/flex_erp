e1ead9d5bd08b96b01dbf0f053a77a3011238fe9
grums <dylangrum@hotmail.fr>
2026-01-19 21:26:58 +0100

    Storybook now has dark mode

--- libs/shared/ui/.storybook/preview.ts
+++ libs/shared/ui/.storybook/preview.ts
@@ -1,6 +1,31 @@
 import type { Preview } from '@storybook/angular';
 
 const preview: Preview = {
+  globalTypes: {
+    theme: {
+      description: 'Global theme for components',
+      defaultValue: 'dark',
+      toolbar: {
+        icon: 'circlehollow',
+        items: [
+          { value: 'light', title: 'Light' },
+          { value: 'dark', title: 'Dark' },
+        ],
+        dynamicTitle: true,
+      },
+    },
+  },
+  decorators: [
+    (story, context) => {
+      if (typeof document !== 'undefined') {
+        const root = document.documentElement;
+        const theme = context.globals['theme'];
+        root.classList.toggle('dark', theme === 'dark');
+      }
+
+      return story();
+    },
+  ],
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
