517746e391a15c870389e1ba32f8159ff0d87e7c
grums <dylangrum@hotmail.fr>
2026-01-31 04:10:36 +0100

    Fixed unit tests

--- apps/api/src/app/app.controller.spec.ts
+++ apps/api/src/app/app.controller.spec.ts
@@ -1,21 +1,16 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 describe('AppController', () => {
-  let app: TestingModule;
+  let controller: AppController;
 
-  beforeAll(async () => {
-    app = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
+  beforeEach(() => {
+    controller = new AppController(new AppService());
   });
 
   describe('getData', () => {
     it('should return "Hello API"', () => {
-      const appController = app.get<AppController>(AppController);
-      expect(appController.getData()).toEqual({ message: 'Hello API' });
+      expect(controller.getData()).toEqual({ message: 'Hello API' });
     });
   });
 });
